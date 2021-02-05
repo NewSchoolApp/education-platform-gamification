509f839bb0c0795b4ee663ad92bc2529bbebe178
Davi de Carvalho <davi_carvalho96@hotmail.com>
2021-02-05 13:26:49 -0300

    feat: calling userRewardsService

--- src/GamificationModule/listener/user-rewards.listener.ts
+++ src/GamificationModule/listener/user-rewards.listener.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { SqsMessageHandler } from '@ssut/nestjs-sqs';
-import AWS from 'aws-sdk';
+import { UserRewardsService } from '../service/user-rewards.service';
 
 @Injectable()
 export class UserRewardsListener {
+  constructor(private readonly service: UserRewardsService) {}
+
   @SqsMessageHandler('startMonthlyRanking', false)
-  public async handleMessage(_message: AWS.SQS.Message) {
-    console.log('message', _message);
+  public async handleMessage() {
+    await this.service.topRankingMonthlyReward();
   }
 }
